038d96b7a5bca49d44a9076c9c740fbd5f207591
joron1827 <61901261+joron1827@users.noreply.github.com>
2024-12-17 23:33:27 +0900

    WIP: added initial set up for fast api

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ilitdangkke-BE
 일있당께 백엔드
+
+
+## 필수 설치 요소 
+* python3
+    * vscode terminal 에서 python 입력시 설치 가능
+* git bash
+
+
+### Window 
---
+++ scripts/run_backend_local.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+build_target="ilitdangkke"
+if [ -z "$build_target" ];then
+    echo "no build target (eventilitdangkke)"
+    exit 1
+fi
